b8d9621452186d3d9627dabe05da0fcb000a7ca0
rjz <rj@versal.com>
2015-06-16 18:24:29 -0700

    Fixes shell escaping

--- tasks/travis_after_success.sh
+++ tasks/travis_after_success.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-[ "$TRAVIS_PULL_REQUEST" == 'false' && "$TRAVIS_BRANCH" == 'master' ] || {
+[[ "$TRAVIS_PULL_REQUEST" == 'false' && "$TRAVIS_BRANCH" == 'master' ]] || {
   echo "Skipping post-build tasks for pull request"
   exit 0;
 }
