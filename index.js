0e35968857da87fd14cb822f10fb1610d7ecc0db
rjz <rj@rjzaworski.com>
2017-01-07 15:52:21 -0800

    Rebuilds gh-pages for commit 05893a28a7dd20f3127891acab0c6f1a3e4d66df

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var transforms = require('./lib/transforms');
+
+/**
+ * Build a configuration object from a list of transformations:
+ *
+ *      var confab = require('confab');
+ *      var config = confab([
+ *        confab.loadJSON([
+ *          './config.' + process.env.NODE_ENV + '.json',
+ *          './config.json'
+ *        ]),
+ *
+ *        confab.defaults({
+ *          role: 'api',
+ *          port: 3200
+ *        })
+ *      ]);
+ *
+ * Later, access the configuration exactly as you would expect.
+ *
+ *      console.log(config.role); // 'api'
+ *
+ * @id confab
+ * @type Function
+ * @param {Array} transforms - a list of transformation functions
+ */
+module.exports = function confab (transforms) {
+  return transforms.reduce(function (memo, fn) {
+    return fn(memo);
+  }, {});
+};
+
+// Extend with known transforms
+for (var f in transforms) {
+  module.exports[f] = transforms[f];
+}
+
