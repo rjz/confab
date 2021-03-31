3142e25b1efb56e03eec163dddb5458a18d4657a
rjz <rj@rjzaworski.com>
2021-03-30 17:38:15 -0700

    Fixes TS declaration

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,10 @@
 type Filter<C extends {}> = (c: C) => C;
 
-const confab: {
+declare const confab: {
   <C extends {}>(filters: Filter<C>[]): C;
   loadJSON<C extends {}>(paths: string[]): Filter<C>;
   defaults<C extends {}>(c: Partial<C>, warn?: boolean): Filter<C>;
+  assign<C extends {}>(k: (keyof C)[]): Filter<C>;
   required<C extends {}>(k: (keyof C)[]): Filter<C>;
   loadEnvironment<C extends {}>(keyMap: Record<string, keyof C>): Filter<C>;
 };
